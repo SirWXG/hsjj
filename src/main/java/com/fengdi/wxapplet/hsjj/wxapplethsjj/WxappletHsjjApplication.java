889e53eb90ae17b9b39c1f21dd8c649c001d51cb
SirWXG <1147798612@qq.com>
2019-04-08 10:21:03 +0800

    first commit

---
+++ src/main/java/com/fengdi/wxapplet/hsjj/wxapplethsjj/WxappletHsjjApplication.java
@@ -0,0 +1,13 @@
+package com.fengdi.wxapplet.hsjj.wxapplethsjj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxappletHsjjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WxappletHsjjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fengdi/wxapplet/hsjj/wxapplethsjj/WxappletHsjjApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fengdi.wxapplet.hsjj.wxapplethsjj;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WxappletHsjjApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
